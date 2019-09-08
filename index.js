1a4cc6934e1a13d0434b17bbce9fbdbabdd78503
wakeupmh <wakeupmh@gmail.com>
2019-09-08 19:34:51 -0300

    adding opencv

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const path = require('path');
+const cv = require('opencv4nodejs');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+
+const FPS = 30;
+const wCap = new cv.VideoCapture(0); //face camera - webcam
+wCap.set(cv.CAP_PROP_FRAME_WIDTH, 300);
+wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 300);
+
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 setInterval(()=>{
-    io.emit('img', 'some data');
-}, 1000);
+    const frame = wCap.read();
+    const img = cv.imencode('.jpg', frame).toString('base64');
+    io.emit('img', img);
+}, 1000/FPS);
 
 server.listen(3000);
